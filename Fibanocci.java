a7427b4ca2715bfd0bb01ee99ac9d9505685c98c
Siddharthan-KC <95135770+Siddharthan-KC@users.noreply.github.com>
2021-11-28 10:54:09 +0530

    Add files via upload

---
+++ Fibanocci.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class Fibanocci {
+public static void main(String args[]) {
+	int n=10,fib=0;
+	for(int i=0; i<=n; i++) {
+		fib=fib +i;		
+	}
+	System.out.println(fib);
+}
+}
