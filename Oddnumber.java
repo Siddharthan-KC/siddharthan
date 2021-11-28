07341c333b33eafd86ff1a54d0591ee77002355e
Siddharthan-KC <95135770+Siddharthan-KC@users.noreply.github.com>
2021-11-28 10:01:34 +0530

    Add files via upload

---
+++ Oddnumber.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Oddnumber {
+public static void main(String args[]) {
+	int n=100, d=0;
+	for(int i=1;i<=n;i++){
+		d= i % 2;
+		if (d!=0){
+			System.out.println(i);
+			
+		}
+	}
+}
+}
